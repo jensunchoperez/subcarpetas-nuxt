32ecff24fb3631ebbfc8366e152eccf4b1436f23
yeisonmorelo <163853055+yeisonmorelo@users.noreply.github.com>
2024-09-25 21:09:20 -0500

    Pagina web sobre Nuxt.js
    Esta pagina web ha sido realizada utilizando el servidor Nuxt.js por los estudiantes: Yeison Morelo, Diana Narvaez y Jander Ensuncho

---
+++ about.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+    <h1>Acerca de la pagina</h1>
+    <p>Esta es la página Acerca de, donde puede encontrar información sobre nuestro sitio.</p>
+    <nuxt-link to="/">Regresar a inicio</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
---
+++ contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Contact Page</h1>
+    <p>Get in touch with us via this page.</p>
+    <nuxt-link to="/">Go back Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactPage'
+}
+</script>
